8ba0f24fccb756035807e2e3c2750d8c29d014a0
utkarsh <utkarshsingh132003@gmail.com>
2025-10-28 20:11:42 +0530

    fix:react-router-dom

--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -1,47 +1,26 @@
-import { createBrowserRouter } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import Meals from "../components/Meals";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Blogs from "../components/Blog";
-import App from "../App";
 import Admin from "../components/Admin";
 import SpecificMeal from "../components/SpecificMeal";
-const Router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/meals",
-        element: <Meals />,
-      },
-      {
-        path: "/blogs",
-        element: <Blogs />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/admin",
-        element: <Admin />,
-      },
-      {
-        path: "/meal/:name",
-        element: <SpecificMeal />,
-      },
-    ],
-  },
-]);
+import App from "../App";
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} >
+      <Route index element={<Home />} />
+      <Route path="meals" element={<Meals />} />
+      <Route path="blogs" element={<Blogs />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="admin" element={<Admin />} />
+      <Route path="meal/:name" element={<SpecificMeal />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default Router;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Router from "./Router/index.js"
 
@@ -8,6 +8,8 @@ import Router from "./Router/index.js"
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={Router}/>
+    <BrowserRouter>
+    <Router/>
+    </BrowserRouter>
   </QueryClientProvider>
 )
